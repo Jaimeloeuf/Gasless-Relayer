132576a0c9fc32f25b16998e3c798cb40a6b1a32
Jaime Loeuf <jaimeloeuf@gmail.com>
2019-10-23 14:53:26 +0800

    Added basic /ping route

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@
  * @fileOverview Run an express server on 2020
  * @requires NPM:express
  */
+
+// Function returns uptime in ms. Self invoking partial application with startup time
+// Start time is recorded before any other code is ran
+const uptime = ((start_time) => () => Date.now() - start_time)(Date.now());
+
 const express = require("express");
 const app = express();
 
@@ -11,10 +16,25 @@ const app = express();
 require("dotenv").config();
 
 /* Mount all the middleware onto the Express app */
+// if (process.env.NODE_ENV === "development") app.use(morgan("tiny")); // HTTP logging
 
 
 /* Mount all the routes onto the Express app */
 app.use("/relayer", require('./routes/relayer'));
+
+
+// Ping Route to check server status
+app.get('/ping', (req, res) => {
+	/*	Things to return to client
+        - Request status
+        - Uptime of the server instance
+    */
+    res.json({
+        // @TODO Remove the hardcoded status number
+        status: 200,
+        uptime: uptime()
+    });
+});
 
 
 const port = process.env.PORT || 2020;
