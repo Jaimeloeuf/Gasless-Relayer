0cdffda32d1823092eac4094586c7c6ee281fa36
Jaime Loeuf <jaimeloeuf@gmail.com>
2019-11-10 13:47:54 +0800

    Updated relayer route module: 1.  Added notes and documentation. 2.  Fixed some minor issues with new linter config. 3.  Added imports of config module and wrapped web3 modules. 4.  Added setup code to setup the signing account and also the creation     of the contract object instance at the start, to prevent re-     instantiation on every call. 5.  Changed endpoint for transaction signing POST route. 6.  Added some notes and scaffolding code for the signing route.

--- src/routes/relayer.js
+++ src/routes/relayer.js
@@ -1,13 +1,26 @@
-'use strict'; // Enforce use of strict verion of JavaScript
+"use strict"; // Enforce use of strict verion of JavaScript
 
 /**
  * Routes for signing transactions
  * @author JJ
  * @module Transactions
+ * 
+ * @notes This code is created with reference to many sources, links below:
+ * @notes https://medium.com/blockchannel/life-cycle-of-an-ethereum-transaction-e5c66bae0f6e
  */
 
 const express = require("express");
 const router = express.Router();
+const config = require("../config");
+const { Web3, web3 } = require("../web3");
+
+
+/** @notice Setup code */
+const signerAcc = web3.eth.accounts.privateKeyToAccount(config.private_key);
+
+// Create contract object
+const abi = require("../../build/abi.json");
+const contract = new Web3.eth.Contract(abi);
 
 
 /**
@@ -24,13 +37,13 @@ const router = express.Router();
  * // "Relayer base API endpoint"
  */
 router.get("/", (req, res) => {
-    res.send("Relayer base API endpoint");
+	res.send("Relayer base API endpoint");
 });
 
 
 /**
  * Signs a incoming transaction with private key to pay for user's Gas.
- * @name POST /relayer
+ * @name POST /relayer/signTx
  * @function
  * @param {string} path relative express path
  * @param {callback} middleware express middleware
@@ -50,8 +63,38 @@ router.get("/", (req, res) => {
  * @todo 5. Error handling in the case of no funds in the private key
  * @todo 6: Add protection via options to express.json
  */
-router.post("/", express.json(), async (req, res) => {
-    res.json(req.body);
+router.post("/signTx", express.json(), async (req, res) => {
+
+	/** @notice Parse out data from request body */
+	const { to } = req.body;
+
+	/** 
+	 * @notice Create a raw Transaction first
+	 * 
+	 * @notice Use transaction count to calculate the nonce
+	 * @notice The gasPrice and gasLimit numbers are temporarily hard coded for now
+	 * @notice Value will always be 0, because the relayer will not be sending ETH to the contract
+	 * 
+	 * @Todo Remove the hard coded gasPrice and gasLimit. These values are actually optional
+	 */
+	const rawTx = {
+		nonce: web3.toHex(web3.eth.getTransactionCount(signerAcc)),
+		gasPrice: web3.toHex(100000000000),
+		gasLimit: web3.toHex(140000),
+		to,
+		value: web3.toHex(0),
+		// Encode the ABI of the method and the arguements
+		// data: myContract.methods.myMethod(arg, arg2).encodeABI()
+	};
+
+	/** 
+	 * @notice Sign the transaction 
+	 * @TODO test if this works
+	*/
+	const signedTx = signerAcc.signTransaction(rawTx);
+
+	/** @notice Send back the signed transaction */
+	res.json(signedTx);
 });
 
 
